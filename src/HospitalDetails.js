af78568f8945f432826b6c3f408eeb69baf3fdd7
Gerroar <germanariasrodriguez@gmail.com>
2022-07-08 01:35:48 +0200

    Finish hospital details layout and design

--- src/HospitalDetails.js
+++ src/HospitalDetails.js
@@ -3,94 +3,59 @@ import {
   View,
   Text,
   StyleSheet,
-  Dimensions,
   Linking,
   TouchableHighlight,
   Platform,
+  Dimensions,
+  ScrollView,
 } from 'react-native';
 import MapView from 'react-native-maps';
 import openMap from 'react-native-open-maps';
-
 import { MaterialCommunityIcons, MaterialIcons, FontAwesome } from '@expo/vector-icons';
 
 import Constants from './Constants';
 
-//https://blog.logrocket.com/applying-box-shadows-in-react-native/
-
-const generateBoxShadow = (
-  xOffset,
-  yOffset,
-  shadowColorIos,
-  shadowOpacity,
-  shadowRadius,
-  elevation,
-  shadowColorAndroid
-) => {
-  if (Platform.OS === 'ios') {
-    styles.boxShadow = {
-      shadowColor: shadowColorIos,
-      shadowOffset: { width: xOffset, height: yOffset },
-      shadowOpacity,
-      shadowRadius,
-    };
-  } else if (Platform.OS === 'android') {
-    styles.boxShadow = {
-      elevation,
-      shadowColor: shadowColorAndroid,
-    };
+function formatPhoneNumber(phoneNumberString) {
+  const cleaned = `${phoneNumberString}`.replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
   }
-};
+  return null;
+}
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'ghostwhite',
-  },
+function DetailCard({ onPress, children }) {
+  return (
+    <TouchableHighlight
+      style={{ borderRadius: 10, marginTop: 20 }}
+      onPress={onPress}
+      underlayColor="#DDDDDD"
+    >
+      {children}
+    </TouchableHighlight>
+  );
+}
 
-  bedContainer: {
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    marginTop: 15,
-    marginBottom: 5,
-    marginLeft: '3.5%',
-    height: '23%',
-    width: '93%',
-    borderRadius: 10,
-  },
-  bedIcon: {
-    alignItems: 'center',
-  },
-  cardContainer: {
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 5,
-    marginBottom: 5,
-    marginLeft: '3.5%',
-    height: '15%',
-    width: '93%',
-    borderRadius: 10,
-    infoView: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      infoIcon: {
-        paddingRight: 20,
-      },
-    },
-  },
-  boxShadow: {},
-  mapView: {
-    alignSelf: 'stretch',
-    height: '100%',
-  },
-});
 export default function HospitalDetails({ route }) {
-  const [hospitalInfo, setHospitalInfo] = useState({});
+  const [hospitalInfo, setHospitalInfo] = useState(null);
+  const [isPortrait, setIsPortrait] = useState(false);
   const { id } = route.params;
+
+  useEffect(() => {
+    const { width, height } = Dimensions.get('window');
+    setIsPortrait(width <= height);
+  }, []);
+
+  useEffect(() => {
+    const eventSubscription = Dimensions.addEventListener('change', (dimensions) => {
+      const { width, height } = dimensions.window;
+      setIsPortrait(width <= height);
+    });
+
+    return () => {
+      eventSubscription.remove();
+    };
+  }, []);
 
   useEffect(() => {
     fetch(`${Constants.apiUrl}/api/${id}`)
@@ -99,71 +64,125 @@ export default function HospitalDetails({ route }) {
       .catch((err) => console.error(err));
   }, [id]);
 
-  generateBoxShadow(0, 2, '#171717', 0.25, 3.84, 5, '#171717');
-
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       {!hospitalInfo ? (
         <Text>Loading....</Text>
       ) : (
-        <View>
-          <View style={styles.bedContainer}>
-            <View style={styles.bedIcon}>
-              <MaterialCommunityIcons name="bed-empty" size={24} color="black" />
-              <Text>{hospitalInfo.available_beds} Available beds</Text>
-            </View>
-            <View style={styles.bedIcon}>
-              <MaterialCommunityIcons name="bed" size={24} color="black" />
-              <Text>{hospitalInfo.total_beds} Total beds</Text>
-            </View>
-          </View>
-          <TouchableHighlight
-            style={[styles.cardContainer, styles.boxShadow]}
-            onPress={() => {
-              Linking.openURL(`tel:${hospitalInfo.phone}`);
+        <View style={[styles.container, { flexDirection: isPortrait ? 'column' : 'row' }]}>
+          <MapView
+            style={{
+              flex: 1,
+            }}
+            showsUserLocation
+            followUserLocation
+            showsPointsOfInterest={false}
+            region={{
+              latitude: hospitalInfo.latitude,
+              longitude: hospitalInfo.longitude,
+              latitudeDelta: 0.02,
+              longitudeDelta: 0.02,
             }}
           >
-            <View style={styles.cardContainer.infoView}>
-              <FontAwesome
-                name="phone"
-                size={24}
-                color="black"
-                style={styles.cardContainer.infoView.infoIcon}
-              />
-              <Text>{hospitalInfo.phone}</Text>
-            </View>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={[styles.cardContainer, styles.boxShadow]}
-            onPress={() => {
-              openMap({ latitude: 43.72255759828326, longitude: -79.37402099746308 });
-            }}
+            <MapView.Marker
+              title={hospitalInfo.name}
+              description="HOSPITAL"
+              coordinate={{ latitude: hospitalInfo.latitude, longitude: hospitalInfo.longitude }}
+            />
+          </MapView>
+          <ScrollView
+            style={{ flex: 1, paddingLeft: isPortrait ? 0 : 15, paddingTop: isPortrait ? 15 : 0 }}
           >
-            <View style={styles.cardContainer.infoView}>
-              <MaterialIcons
-                name="add-location"
-                size={24}
-                color="black"
-                style={styles.cardContainer.infoView.infoIcon}
-              />
-              <Text>{hospitalInfo.address}</Text>
+            <View style={styles.bedContainer}>
+              <View style={styles.bedIcons}>
+                <FontAwesome name="hourglass" size={24} color="black" />
+                <Text>Waiting time</Text>
+                <Text>{hospitalInfo.wait_time} min</Text>
+              </View>
+              <View style={styles.bedIcons}>
+                <MaterialCommunityIcons name="bed-empty" size={30} color="black" />
+                <Text>Available beds</Text>
+                <Text>{hospitalInfo.available_beds}</Text>
+              </View>
+              <View style={styles.bedIcons}>
+                <MaterialCommunityIcons name="bed" size={30} color="black" />
+                <Text>Total beds</Text>
+                <Text>{hospitalInfo.total_beds}</Text>
+              </View>
             </View>
-          </TouchableHighlight>
+            <DetailCard onPress={() => Linking.openURL(`tel:${hospitalInfo.phone}`)}>
+              <View style={styles.card}>
+                <FontAwesome style={styles.infoIcon} name="phone" size={35} color="black" />
+                <Text style={styles.infoTxt}>{formatPhoneNumber(hospitalInfo.phone)}</Text>
+              </View>
+            </DetailCard>
+            <DetailCard
+              onPress={() =>
+                openMap({
+                  query: hospitalInfo.address,
+                })
+              }
+            >
+              <View style={styles.card}>
+                <MaterialIcons
+                  name="add-location"
+                  size={35}
+                  color="black"
+                  style={styles.infoIcon}
+                />
+                <Text style={styles.infoTxt}>{hospitalInfo.address}</Text>
+              </View>
+            </DetailCard>
+          </ScrollView>
         </View>
       )}
-
-      <MapView
-        style={styles.mapView}
-        showsUserLocation
-        followUserLocation
-        showsPointsOfInterest={false}
-      >
-        <MapView.Marker
-          title={hospitalInfo.name}
-          description="HOSPITAL"
-          coordinate={{ latitude: 43.7248743, longitude: -79.429756 }}
-        />
-      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+  },
+  bedContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    padding: 20,
+    borderRadius: 10,
+  },
+  bedIcons: {
+    alignItems: 'center',
+  },
+  infoIcon: {
+    marginLeft: '20%',
+  },
+  infoTxt: {
+    marginLeft: '10%',
+    fontSize: 20,
+    flexShrink: 1,
+  },
+  card: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    borderRadius: 10,
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+      },
+      android: {
+        elevation: 2,
+      },
+    }),
+  },
+});
